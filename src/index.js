962f16c56b75c6a06a617476aaf193bc8160334e
Godfrey Onyango <godiodhis268@gmail.com>
2024-07-13 16:13:40 +0300

    last

--- src/index.js
+++ src/index.js
@@ -1,4 +1,23 @@
 const init = () => {
+    form=document.querySelector('form');
+    form.addEventListener('submit',function(event){
+        event.preventDefault();
+        const text=document.getElementById('searchByID');
+       
+
+        fetch(`http://localhost:3000/movies/${text.value}`)
+        .then (function(response){
+            return response.json()
+        })
+        .then(function(data){
+            
+          
+            const title=document.querySelector('h4');
+            const summary=document.querySelector('p');
+            title.innerText=data.title;
+            summary.innerText=data.summary;
+        })
+    })
   
 }
 
